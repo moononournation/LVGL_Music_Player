8b5b781562d62b93e826489604cd26b787a61ffc
moononournation <moononournation@gmail.com>
2023-02-26 21:11:12 +0800

    first commit

---
+++ Lvgl_Music_Player/ui.h
@@ -0,0 +1,41 @@
+// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.2.1
+// LVGL VERSION: 8.2.0
+// PROJECT: SquareLine_Project
+
+#ifndef _SQUARELINE_PROJECT_UI_H
+#define _SQUARELINE_PROJECT_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined __has_include
+  #if __has_include("lvgl.h")
+    #include "lvgl.h"
+  #elif __has_include("lvgl/lvgl.h")
+    #include "lvgl/lvgl.h"
+  #else
+    #include "lvgl.h"
+  #endif
+#else
+  #include "lvgl.h"
+#endif
+
+extern lv_obj_t *ui_Screen1;
+extern lv_obj_t *ui_Spinner1;
+extern lv_obj_t *ui_Label2;
+
+
+
+
+LV_FONT_DECLARE( ui_font_NotoSerifCJKhk);
+
+
+void ui_init(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
