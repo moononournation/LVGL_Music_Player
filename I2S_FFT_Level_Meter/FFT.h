b650f45d9c48eb9c3e3bb5fb96886bbbca0e9129
moononournation <moononournation@gmail.com>
2023-04-09 18:48:27 +0800

    fine tune

--- I2S_FFT_Level_Meter/FFT.h
+++ I2S_FFT_Level_Meter/FFT.h
@@ -2,7 +2,7 @@
  * Extract from:
  * https://github.com/m5stack/M5Unified/blob/master/examples/Advanced/MP3_with_ESP8266Audio/MP3_with_ESP8266Audio.ino
  */
-#define FFT_SIZE 128
+#define FFT_SIZE 512
 class fft_t
 {
   float _wr[FFT_SIZE + 1];
@@ -18,16 +18,16 @@ public:
 #ifndef M_PI
 #define M_PI 3.141592653
 #endif
-    _ie = logf( (float)FFT_SIZE ) / log(2.0) + 0.5;
+    _ie = logf((float)FFT_SIZE) / log(2.0) + 0.5;
     static constexpr float omega = 2.0f * M_PI / FFT_SIZE;
     static constexpr int s4 = FFT_SIZE / 4;
     static constexpr int s2 = FFT_SIZE / 2;
-    for ( int i = 1 ; i < s4 ; ++i)
+    for (int i = 1; i < s4; ++i)
     {
-    float f = cosf(omega * i);
+      float f = cosf(omega * i);
       _wi[s4 + i] = f;
       _wi[s4 - i] = f;
-      _wr[     i] = f;
+      _wr[i] = f;
       _wr[s2 - i] = -f;
     }
     _wi[s4] = _wr[0] = 1;
@@ -35,23 +35,23 @@ public:
     size_t je = 1;
     _br[0] = 0;
     _br[1] = FFT_SIZE / 2;
-    for ( size_t i = 0 ; i < _ie - 1 ; ++i )
+    for (size_t i = 0; i < _ie - 1; ++i)
     {
-      _br[ je << 1 ] = _br[ je ] >> 1;
+      _br[je << 1] = _br[je] >> 1;
       je = je << 1;
-      for ( size_t j = 1 ; j < je ; ++j )
+      for (size_t j = 1; j < je; ++j)
       {
         _br[je + j] = _br[je] + _br[j];
       }
     }
   }
 
-  void exec(const int16_t* in)
+  void exec(const int16_t *in)
   {
     memset(_fi, 0, sizeof(_fi));
-    for ( size_t j = 0 ; j < FFT_SIZE / 2 ; ++j )
+    for (size_t j = 0; j < FFT_SIZE / 2; ++j)
     {
-      float basej = 0.25 * (1.0-_wr[j]);
+      float basej = 0.25 * (1.0 - _wr[j]);
       size_t r = FFT_SIZE - j - 1;
 
       /// perform han window and stereo to mono convert.
@@ -81,47 +81,46 @@ public:
           _fi[m] = _fi[l] - Wxmi;
           _fr[l] += Wxmr;
           _fi[l] += Wxmi;
-        } while ( ++k < ke) ;
-      } while ( ++j < je );
-    } while ( ++i < _ie );
+        } while (++k < ke);
+      } while (++j < je);
+    } while (++i < _ie);
   }
 
   uint32_t get(size_t index)
   {
-    return (index < FFT_SIZE / 2) ? (uint32_t)sqrtf(_fr[ index ] * _fr[ index ] + _fi[ index ] * _fi[ index ]) : 0u;
+    return (index < FFT_SIZE / 2) ? (uint32_t)sqrtf(_fr[index] * _fr[index] + _fi[index] * _fi[index]) : 0u;
   }
 };
 
-static constexpr size_t WAVE_SIZE = 1470;
+static constexpr size_t WAVE_SIZE = (44100 / 30);
 static fft_t fft;
-static uint16_t prev_h[(FFT_SIZE / 2)+1];
-static uint16_t peak_y[(FFT_SIZE / 2)+1];
+static uint16_t prev_h[(FFT_SIZE / 2) + 1];
+static uint16_t peak_y[(FFT_SIZE / 2) + 1];
 static uint32_t raw_data[WAVE_SIZE];
 static size_t raw_data_idx = 0;
 
 static uint16_t level_color[] = {
-  RGB565(0xcd, 0x19, 0x22),
-  RGB565(0xcd, 0x22, 0x10),
-  RGB565(0xcd, 0x2a, 0x10),
-  RGB565(0xd6, 0x5a, 0x00),
-  RGB565(0xd6, 0x66, 0x01),
-  RGB565(0xd6, 0x73, 0x01),
-  RGB565(0xc6, 0x7b, 0x09),
-  RGB565(0xde, 0xa5, 0x19),
-  RGB565(0xd6, 0xb5, 0x22),
-  RGB565(0xbc, 0xde, 0x2a),
-  RGB565(0x93, 0xde, 0x22),
-  RGB565(0x29, 0xce, 0x10),
-  RGB565(0x32, 0xbe, 0x10),
-  RGB565(0x37, 0xb6, 0x0e),
-  RGB565(0x31, 0x9c, 0x0a),
-  RGB565(0x28, 0x94, 0x01)
-};
+    RGB565(0xcd, 0x19, 0x22),
+    RGB565(0xcd, 0x22, 0x10),
+    RGB565(0xcd, 0x2a, 0x10),
+    RGB565(0xd6, 0x5a, 0x00),
+    RGB565(0xd6, 0x66, 0x01),
+    RGB565(0xd6, 0x73, 0x01),
+    RGB565(0xc6, 0x7b, 0x09),
+    RGB565(0xde, 0xa5, 0x19),
+    RGB565(0xd6, 0xb5, 0x22),
+    RGB565(0xbc, 0xde, 0x2a),
+    RGB565(0x93, 0xde, 0x22),
+    RGB565(0x29, 0xce, 0x10),
+    RGB565(0x32, 0xbe, 0x10),
+    RGB565(0x37, 0xb6, 0x0e),
+    RGB565(0x31, 0x9c, 0x0a),
+    RGB565(0x28, 0x94, 0x01)};
 
 static void init_peak_array()
 {
-  int i = (FFT_SIZE / 2)+1;
-  while(i--)
+  int i = (FFT_SIZE / 2) + 1;
+  while (i--)
   {
     peak_y[i] = INT16_MAX - 1;
   }
@@ -130,18 +129,27 @@ static void init_peak_array()
 static void draw_fft_level_meter(Arduino_GFX *meter_gfx)
 {
   size_t bw = 4;
-  if (bw < 3) { bw = 3; }
+  if (bw < 3)
+  {
+    bw = 3;
+  }
   int32_t fft_height = 17;
   int32_t fft_heightc = fft_height >> 2;
   size_t xe = 19;
-  if (xe > (FFT_SIZE/2)) { xe = (FFT_SIZE/2); }
+  if (xe > (FFT_SIZE / 2))
+  {
+    xe = (FFT_SIZE / 2);
+  }
 
   for (size_t bx = 0; bx <= xe; ++bx)
   {
     size_t x = bx * bw;
     int32_t f = fft.get(bx);
-    int32_t h = (f * fft_height) >> 14;
-    if (h >= fft_height) { h = fft_height - 1; }
+    int32_t h = (f * fft_height) >> 16;
+    if (h >= fft_height)
+    {
+      h = fft_height - 1;
+    }
     int32_t y = fft_height - h + 1;
     int32_t ph = prev_h[bx];
     if (h != ph)
